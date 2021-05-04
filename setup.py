e68ae7ce3c9bad61911003621a156d8fa4f3f28f
David Duran Cunill <david.duran@blackdata.es>
2021-05-04 20:23:12 +0200

    2o commit

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='cine',
-    packages=['cine'],  # Mismo nombre que en la estructura de carpetas de arriba
-    version='0.1',
+    name='M6UF4Cine',
+    packages=['cine/cine'],  # Mismo nombre que en la estructura de carpetas de arriba
+    version='0.3',
     license='LGPL v3',  # La licencia que tenga tu paquete
     description='La cosa de UF4 de M6',
     author='David Duran Cunill',
     author_email='ranaMarrana@gmail.com',
     url='https://github.com/daviduvi99/M6UF4Cine',  # Usa la URL del repositorio de GitHub
-    download_url='https://github.com/RDCH106/parallel_foreach_submodule/archive/v0.1.tar.gz',
+    download_url='https://github.com/daviduvi99/M6UF4Cine/archive/refs/heads/master.zip',
     # Te lo explico a continuación
     keywords='test example develop',  # Palabras que definan tu paquete
     classifiers=['Programming Language :: Python',
